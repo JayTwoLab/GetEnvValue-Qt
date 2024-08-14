948702aeffea243e089e1aea81e6eca985b27743
Jay Two <j2doll@gmail.com>
2024-08-14 20:00:01 +0900

    Update GetEnv.h

--- GetEnv.h
+++ GetEnv.h
@@ -2,8 +2,6 @@
 #ifndef QT_GET_ENV_H
 #define QT_GET_ENV_H
 
-#ifdef Q_QT_VERSION 
-
 #if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0) // using Qt (6 or higher version is recommended)
 
 #include <QtGlobal>
@@ -17,10 +15,8 @@ namespace j2
     namespace Qt6
     {
 
-        ///
         /// \brief Get path list of OS
         /// \return path list of OS
-        ///
         QStringList GetOSPath();
 
     } // namespace Qt6
@@ -28,7 +24,5 @@ namespace j2
 } // namespace j2
 
 #endif // #if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0) 
-
-#endif // #ifdef Q_QT_VERSION
 
 #endif // #ifndef QT_GET_ENV_H
